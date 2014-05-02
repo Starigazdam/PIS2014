fc4a9821fd0f6b500dd6da565ea9f8db06a7cf08
starigazdam <starigazdam@gamil.com>
2014-05-02 13:31:16 +0200

    Added PatientAccountManager
    Signed-off-by: starigazdam <starigazdam@gamil.com>

---
+++ ZubniOrdinace/src/org/fit/pis/service/PatientAccountManager.java
@@ -0,0 +1,37 @@
+package org.fit.pis.service;
+
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.fit.pis.data.PatientAccount;
+
+@Stateless
+public class PatientAccountManager {
+    @PersistenceContext
+    private EntityManager em;
+    
+    public PatientAccount find(String id)
+    {
+    	return em.find(PatientAccount.class, id);
+    }
+
+    public void save(PatientAccount pa)
+    {
+    	em.merge(pa);
+    }
+	
+    public void remove(PatientAccount pa)
+    {
+    	em.remove(em.merge(pa));
+    }
+    
+    @SuppressWarnings("unchecked")
+    public List<PatientAccount> findAll()
+    {
+    	return em.createQuery("SELECT pa FROM PatientAccount pa").getResultList();
+    }
+
+}
