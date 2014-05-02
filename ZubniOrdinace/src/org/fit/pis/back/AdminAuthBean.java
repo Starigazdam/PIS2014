36581ae217466258e16cd38c8f1e0d48fdb46b7b
Stanislav Láznička <slaz@seznam.cz>
2014-05-02 11:44:28 +0200

    Added admin auth bean
    Signed-off-by: Stanislav Láznička <slaz@seznam.cz>

---
+++ ZubniOrdinace/src/org/fit/pis/back/AdminAuthBean.java
@@ -0,0 +1,79 @@
+package org.fit.pis.back;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
+import javax.ejb.EJB;
+import javax.enterprise.context.SessionScoped;
+
+import org.fit.pis.service.EmployeeManager;
+import org.fit.pis.data.Employee;
+
+@ManagedBean
+@SessionScoped
+public class AdminAuthBean {
+	private boolean authorized;
+	private String login;
+	private String password;
+	@EJB
+	private EmployeeManager mng;
+	
+    public AdminAuthBean()
+    {
+        authorized = false;
+    }
+
+    public boolean isAuthorized()
+    {
+        return authorized;
+    }
+
+    public void setAuthorized(boolean authorized)
+    {
+        this.authorized = authorized;
+    }
+    
+    public String getLogin()
+    {
+        return login;
+    }
+
+    public void setLogin(String login)
+    {
+        this.login = login;
+    }
+
+    public String getPassword()
+    {
+        return password;
+    }
+    
+    public void setPassword(String password)
+    {
+        this.password = password;
+    }
+
+    public String actionLogout()
+    {
+        authorized = false;
+        return "logout";
+    }
+    
+    public String actionLogin()
+    {
+    	Employee temp;
+    	temp = mng.find(login);
+        if ((temp != null) && temp.getPassword().equals(password))
+        {
+            authorized = true;
+            return "login";
+        }
+        else
+        {
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Invalid login"));
+            return "failed";
+        }
+    }
+
+
+}
