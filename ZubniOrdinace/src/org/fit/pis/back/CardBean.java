9cc15d1395ca21df17c96142514c3b695f68a5a0
starigazdam <starigazdam@gmail.com>
2014-05-03 19:47:04 +0200

    add procedure
    Signed-off-by: starigazdam <starigazdam@gmail.com>

--- ZubniOrdinace/src/org/fit/pis/back/CardBean.java
+++ ZubniOrdinace/src/org/fit/pis/back/CardBean.java
@@ -2,6 +2,7 @@ package org.fit.pis.back;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Vector;
 
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
@@ -9,6 +10,8 @@ import javax.faces.bean.SessionScoped;
 
 import org.fit.pis.data.Card;
 import org.fit.pis.data.Illness;
+import org.fit.pis.data.Procedure;
+import org.fit.pis.data.Treatment;
 import org.fit.pis.service.CardManager;
 import org.richfaces.component.UIDataTable;
 
@@ -19,11 +22,16 @@ public class CardBean {
 	private CardManager cardMgr;
 	private Card card;
 	private Illness illness; 
-    private UIDataTable listTable;
+	private Treatment treat;
+	private Procedure proc;
+
+	private UIDataTable listTable;
 	
 	public CardBean() {
 		card = new Card();
 		illness = new Illness();
+		treat = new Treatment();
+		proc = new Procedure();
 	}
 
 	public Card getCard() {
@@ -40,6 +48,22 @@ public class CardBean {
 
 	public void setIllness(Illness illness) {
 		this.illness = illness;
+	}
+	
+	public Treatment getTreat() {
+		return treat;
+	}
+
+	public void setTreat(Treatment treat) {
+		this.treat = treat;
+	}
+	
+	public Procedure getProc() {
+		return proc;
+	}
+
+	public void setProc(Procedure proc) {
+		this.proc = proc;
 	}
 
 	public List<Card> getCards() {
@@ -94,5 +118,31 @@ public class CardBean {
 		illness.setPatient(card);
         card.getIllnesses().add(illness);
         return "ok";
+	}
+	
+	public String actionTreatNew() {
+		treat = new Treatment();
+		return "newtreat";
+	}
+	
+	public String actionTreatAdd() {
+		treat.setPatient(card);
+		treat.setDate(new Date());
+		card.getTreatmentHistory().add(treat);
+		return "treatadd";
+	}
+	
+	public String actionProcNew() {
+		proc = new Procedure();
+		return "newproc";
+	}
+	
+	public String actionProcAdd() {
+		//if (treat.getProcedures() == null) return "fail";
+		proc.setTreatment(treat);
+		proc.setName(proc.getCode().name());
+		treat.setProcedures(new Vector<Procedure>());
+		treat.getProcedures().add(proc);
+		return "addproc";
 	}
 }
--- ZubniOrdinace/src/org/fit/pis/data/Procedure.java
+++ ZubniOrdinace/src/org/fit/pis/data/Procedure.java
@@ -16,9 +16,52 @@ public class Procedure {
 	private long id;
 	
 	private ProcedureCode code;
-	private String name; // TODO: nelze toto ziskat priamo z enumu cez nejaky konstruktor?
+	// DEPRECATED
+	private String name; // TODO: lze ziskat z enumu
 	private int cost;
 	
 	@ManyToOne(fetch=EAGER)
 	private Treatment treatment;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public ProcedureCode getCode() {
+		return code;
+	}
+
+	public void setCode(ProcedureCode code) {
+		this.code = code;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getCost() {
+		return cost;
+	}
+
+	public void setCost(int cost) {
+		this.cost = cost;
+	}
+
+	public Treatment getTreatment() {
+		return treatment;
+	}
+
+	public void setTreatment(Treatment treatment) {
+		this.treatment = treatment;
+	}
+	
+	
 }
