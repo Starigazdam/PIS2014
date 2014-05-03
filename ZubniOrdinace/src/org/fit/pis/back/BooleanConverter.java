8a7c1b896aa5b7dffdb2f847590eee216d6c753c
Stanislav Láznička <slaz@seznam.cz>
2014-05-03 16:04:16 +0200

    Boolean Converter

---
+++ ZubniOrdinace/src/org/fit/pis/back/BooleanConverter.java
@@ -0,0 +1,36 @@
+package org.fit.pis.back;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.ConverterException;
+import javax.faces.convert.FacesConverter;
+
+@FacesConverter(value="booleanConverter")
+public class BooleanConverter implements Converter {
+		
+	@Override
+	public Object getAsObject(FacesContext arg0, UIComponent component, String value)
+	{
+		if (value.length() == 4 && value.equalsIgnoreCase("true"))
+		{
+			return true;
+		}
+		else if(value.length() == 5 && value.equalsIgnoreCase("false")) {
+			return false;
+		}
+		else
+			throw new ConverterException("Invalid ID format");
+	}
+
+	@Override
+	public String getAsString(FacesContext arg0, UIComponent component, Object value)
+	{
+		try {
+			if(value.equals(true)) return "true";
+			else return "false";
+		} catch (ClassCastException e) {
+			throw new ConverterException("Invalid ID value");
+		}
+	}
+}
