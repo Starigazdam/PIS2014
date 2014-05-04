7f2ad4a98632975d44ca7a7d8d075f56c39c80a8
Stanislav Láznička <slaz@seznam.cz>
2014-05-04 11:27:04 +0200

    Added timeouts
    Signed-off-by: Stanislav Láznička <slaz@seznam.cz>

--- ZubniOrdinace/src/org/fit/pis/back/AdminAuthFilter.java
+++ ZubniOrdinace/src/org/fit/pis/back/AdminAuthFilter.java
@@ -15,17 +15,29 @@ public class AdminAuthFilter implements Filter {
 	 {
 	     HttpSession session = ((HttpServletRequest) request).getSession();
 	     AdminAuthBean auth = (AdminAuthBean) session.getAttribute("adminAuthBean");
-	     if (auth != null && auth.isAuthorized())
-	     {
-	         chain.doFilter(request, response);
+	     if(!session.isNew()) {
+	    	 if (auth != null && auth.isAuthorized())
+		     {
+		         chain.doFilter(request, response);
+		     }
+		     else
+		     {
+		         response.setContentType("text/html");
+		         PrintWriter out = response.getWriter();
+		         out.println("<html><head><meta http-equiv=\"refresh\" content=\"2; url=../admlogin.xhtml\" />");
+		         out.println("<title>Bad login.</title></head><body>");
+		         out.println("<h1>Access denied</h1>");
+		         out.println("Access denied. Redirecting back to <a href=\"../admlogin.xhtml\">Login</a>.");
+		         out.println("</body></html>");
+		     }
 	     }
-	     else
-	     {
-	         response.setContentType("text/html");
+	     else {
+	    	 response.setContentType("text/html");
 	         PrintWriter out = response.getWriter();
-	         out.println("<html><head><title>Bad login</title></head><body>");
-	         out.println("<h1>Access denied</h1>");
-	         out.println("Access denied. <a href=\"../admlogin.xhtml\">Try again</a>.");
+	         out.println("<html><head><meta http-equiv=\"refresh\" content=\"2; url=../admlogin.xhtml\" />");
+	         out.println("<title>Access denied.</title></head><body>");
+	         out.println("<h1>You are not logged in.</h1>");
+	         out.println("Redirecting you to <a href=\"../admlogin.xhtml\">Login.</a>.");
 	         out.println("</body></html>");
 	     }
 	 }
